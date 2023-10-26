d394f3d86db02ee182f9c3efce4e9611c110203d
Thanaphon <thanaphon.p@i3gateway.com>
2023-10-26 09:48:09 +0700

    login endpoint

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -1,6 +1,7 @@
 import { ClientQuery } from "@/scripts/db";
 import { NextResponse } from "next/server";
 import bcrypt from "bcrypt";
+import { SignJWT } from "jose";
 
 export async function POST(request: Request) {
   const json = await request.json();
@@ -13,11 +14,24 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: "user not found" }, { status: 404 });
   }
 
-  //   const user = result.rows[0];
-  //   const match = await bcrypt.compare(json.password, user.password);
-  //   if (!match) {
-  //     return NextResponse.json({ error: "invalid credentials" }, { status: 401 });
-  //   }
+  const user = result.rows[0];
+  const match = await bcrypt.compare(json.password, user.password);
+  if (!match) {
+    return NextResponse.json({ error: "invalid credentials" }, { status: 401 });
+  }
 
-  return NextResponse.json({ data: result.rows[0] });
+  const token = await new SignJWT({})
+    .setProtectedHeader({ alg: "HS256" })
+    .setSubject(user.id)
+    .setIssuedAt()
+    .setExpirationTime("2w")
+    .sign(new TextEncoder().encode("my-jwt-secret"));
+  const response = NextResponse.json({ msg: "login success" });
+  response.cookies.set("jwt-token", token, {
+    sameSite: "strict",
+    httpOnly: true,
+    secure: true,
+  });
+
+  return response;
 }
