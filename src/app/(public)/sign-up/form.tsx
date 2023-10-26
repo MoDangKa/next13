2ba393482bed8faf781646c07eb8cdd2da738b4a
Thanaphon <thanaphon.p@i3gateway.com>
2023-10-26 11:55:33 +0700

    sign up endpoint

---
+++ src/app/(public)/sign-up/form.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { FormEvent, useState } from "react";
+
+function Form() {
+  const router = useRouter();
+  const [username, setUsername] = useState<undefined | string>("");
+  const [password, setPassword] = useState<undefined | string>("");
+
+  async function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    const result = await fetch("/api/login", {
+      method: "post",
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (result.ok) {
+      router.push("/feed");
+    } else {
+      alert("log in failed");
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-2 p-5 max-w-xs w-full bg-slate-800 rounded-lg"
+    >
+      <div className="text-center">
+        <h3 className="font-semibold">Sign In</h3>
+      </div>
+
+      <div className="my-3">
+        <hr />
+      </div>
+
+      <div>
+        <div className="flex flex-col gap-2">
+          <label>Username</label>
+          <input
+            className="text-black p-3 border border-slate-700 rounded-lg"
+            type="text"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
+            placeholder="Username"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-2">
+          <label>Password</label>
+          <input
+            className="text-black p-3 border border-slate-700 rounded-lg"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            placeholder="Password"
+            required
+          />
+        </div>
+      </div>
+
+      <button type="submit" className="mt-4 bg-slate-900 p-3 rounded-lg">
+        Sign In
+      </button>
+    </form>
+  );
+}
+
+export default Form;
---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,25 @@
+import { ClientQuery } from "@/scripts/db";
+import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const json = await request.json();
+  const result = await ClientQuery(
+    "select id, username from users where username ilike $1",
+    [json.username]
+  );
+
+  if (result.rowCount > 0) {
+    return NextResponse.json({ error: "user already exists" }, { status: 400 });
+  }
+
+  const salt = parseInt(process.env.NEXT_PUBLIC_DB_SALT!);
+  const hash = await bcrypt.hash(json.password, salt);
+
+  await ClientQuery(
+    "insert into public.users (username, password) values ($1, $2)",
+    [json.username, hash]
+  );
+
+  return NextResponse.json({ msg: "registration success" }, { status: 201 });
+}
