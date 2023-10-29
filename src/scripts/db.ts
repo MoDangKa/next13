6f1efa5053db806ec1ba4ca1d6c7fc3c474b67fc
Thanaphon <thanaphon.p@i3gateway.com>
2023-10-29 15:42:02 +0700

    update code

--- src/scripts/db.ts
+++ src/scripts/db.ts
@@ -5,12 +5,19 @@ const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 export async function Client(): Promise<pg.Client> {
+  if (process.env.POSTGRES_URL) {
+    const client = new pg.Client({
+      connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+    });
+    return client;
+  }
+
   const config: ClientConfig = {
-    host: process.env.NEXT_PUBLIC_DB_HOST,
-    port: parseInt(process.env.NEXT_PUBLIC_DB_PORT!),
-    database: process.env.NEXT_PUBLIC_DB_DATABASE,
-    user: process.env.NEXT_PUBLIC_DB_USER,
-    password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: parseInt(process.env.POSTGRES_PORT!),
+    database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
   };
   const client = new pg.Client(config);
   return client;
