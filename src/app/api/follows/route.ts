d9fbc1b03aceae05b21d4d02e9bc8537961922dc
Thanaphon <thanaphon.p@i3gateway.com>
2023-11-06 11:58:51 +0700

    feat: create follows endpoint

--- src/app/api/follows/route.ts
+++ src/app/api/follows/route.ts
@@ -14,3 +14,24 @@ export async function GET(request: NextRequest) {
 
   return NextResponse.json({ data: result.rows });
 }
+
+export async function POST(request: NextRequest) {
+  const jwtPayload = await getJWTPayload();
+  const json = await request.json();
+
+  const result = await ClientQuery(
+    "select * from follows where user_id = $1 and follower_id = $2",
+    [json.user_id, jwtPayload.sub]
+  );
+
+  if (result.rowCount > 0) {
+    return NextResponse.json({ error: "already following" }, { status: 409 });
+  }
+
+  await ClientQuery(
+    "insert into follows (user_id, follower_id) values ($1, $2)",
+    [json.user_id, jwtPayload.sub]
+  );
+
+  return NextResponse.json({ msg: "follow success" });
+}
